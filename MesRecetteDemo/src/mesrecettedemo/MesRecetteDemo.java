8566b16ad5f812a2bfb59c90c7fad26f9388cef5
JBDEV95 <48797660+Kehoe95@users.noreply.github.com>
2023-05-05 20:30:49 +0200

    Add files via upload

---
+++ MesRecetteDemo/src/mesrecettedemo/MesRecetteDemo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mesrecettedemo;
+
+/**
+ *
+ * @author JBDEV95
+ */
+public class MesRecetteDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Vue vu = new Vue();
+        vu.setVisible(true);
+    }
+    
+}
